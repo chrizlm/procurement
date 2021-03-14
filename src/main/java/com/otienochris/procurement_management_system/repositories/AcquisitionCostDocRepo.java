89eec1930a3c921dfce5999b215c6367ad29de79
chrizlm <42278574+chrizlm@users.noreply.github.com>
2021-03-14 21:42:08 +0300

    Add files via upload

---
+++ src/main/java/com/otienochris/procurement_management_system/repositories/AcquisitionCostDocRepo.java
@@ -0,0 +1,9 @@
+package com.groupwork.Explorers.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.groupwork.Explorers.model.Docs.AcquisitionCostDoc;
+
+public interface AcquisitionCostDocRepo extends JpaRepository<AcquisitionCostDoc, Integer> {
+
+}
